b838b8a42319a7f8dff016e6a44615a4cc0fe46a
Rodrigo Leite <rodrigo.freitas.leite@gmail.com>
2016-03-13 21:56:26 -0300

    starting 11062

---
+++ 11062/Main.cpp
@@ -0,0 +1,12 @@
+/// https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=22&page=show_problem&problem=2003
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	return 0;
+}
