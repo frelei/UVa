021de1d852b237414a821734ec2a988a1d3bf478
Rodrigo Freitas Leite <rodrigo.freitas.leite@gmail.com>
2016-03-27 16:21:32 -0300

    enter 10172

---
+++ 10172/Main.cpp
@@ -0,0 +1,15 @@
+///https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=13&page=show_problem&problem=1113
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
